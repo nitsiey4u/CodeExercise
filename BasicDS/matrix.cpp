b0113c4ec78196fac28661d4101950c8ca677e82
nitsiey4u <nitsiey@yahoo.com>
2020-09-24 20:37:11 +0530

    Nonlinear DS updates

--- BasicDS/matrix.cpp
+++ BasicDS/matrix.cpp
@@ -1,18 +1,26 @@
+/***********************************************/
+/********   Matrix Related Problems   **********/
+/***********************************************/
 #include "stdio.h"
 #include "stdlib.h"
 
-#define MAX_ROWS 5
-#define MAX_COLS 4
-#define MAX_DIRS 4
+// Matrix for diagonal, spiral printing
+#define MAX_ROWS 5    // Matrix 5 x 4
+#define MAX_COLS 4    // Matrix 5 x 4
 
-// Input matrix (static)
-int arr[MAX_ROWS][MAX_COLS] = {
-  {2, 4, 6, 8},
-  {5, 9, 12, 16},
-  {9, 5, 11, 2},
-  {2, 11, 5, 9},
-  {3, 2, 1, 8},
-};
+// Square matrix for add/subtract/multiply operations
+#define TBL_ROWS 3    // Square Matrix 3 x 3
+#define TBL_COLS 3    // Square Matrix 3 x 3
+
+// Binary matrix for island counting - 8 neighbours
+#define BIN_ROWS 5    // Matrix 5 x 5
+#define BIN_COLS 5    // Matrix 5 x 5
+
+// Color coded matrix for flood fill - 4 neighbours
+#define CLR_ROWS 8
+#define CLR_COLS 8
+
+#define MAX_DIRS 4
 
 // Utility to dynamically allocated matrix
 void dynamic_alloc(int** arr) {
@@ -34,12 +42,31 @@ void free_alloc(int** arr) {
 // Utility to display matrix
 void print_matrix(int arr[][MAX_COLS], int rows, int cols) {
   int i, j;
-  printf("\nMatrix: \n");
+  printf("\nNon-Square Matrix: \n");
   for(i=0; i < rows; i++) {
     for(j=0; j < cols; j++) {
       printf("\t%d", arr[i][j]);
     }
     printf("\n");
+  }
+}
+
+// Transpose of matrix
+void matrix_transpose(int mat[MAX_ROWS][MAX_COLS], int rows, int cols) {
+  // Swap col count and row count, along with index values
+  int res[cols][rows];
+  for(int rowIndex = 0; rowIndex < rows; rowIndex++) {
+    for(int colIndex = 0; colIndex < cols; colIndex++) {
+      res[colIndex][rowIndex] = mat[rowIndex][colIndex];
+    }
+  }
+  // Display tranposed matrix
+  printf("\nTranspose Matrix");
+  for(int rowIndex = 0; rowIndex < cols; rowIndex++) {
+    printf("\n");
+    for(int colIndex = 0; colIndex < rows; colIndex++) {
+      printf("\t%d", res[rowIndex][colIndex]);
+    }
   }
 }
 
@@ -51,54 +78,58 @@ void spiral_print(int arr[][MAX_COLS], int rows, int cols) {
   int left_col = 0;
   int right_col = (cols - 1);
   printf("\nSpiral Print: ");
+  // Spiral traversal
   while((top_row <= bottom_row) && (left_col <= right_col)) {
     int row_index, col_index;
-
+    // Right Traverse >
     if(cur_dir == 0) {
       row_index = top_row;
       for(col_index = left_col; col_index <= right_col; col_index++)
         printf("\t%d", arr[row_index][col_index]);
       top_row ++;
     }
-
+    // Down Traverse v
     if(cur_dir == 1) {
       col_index = right_col;
       for(row_index = top_row; row_index <= bottom_row; row_index++)
         printf("\t%d", arr[row_index][col_index]);
       right_col --;
     }
-
+    // Left Traverse <
     if(cur_dir == 2) {
       row_index = bottom_row;
       for(col_index = right_col; col_index >= left_col; col_index--)
         printf("\t%d", arr[row_index][col_index]);
       bottom_row --;
     }
-
+    // Top Traverse ^
     if(cur_dir == 3) {
       col_index = left_col;
       for(row_index = bottom_row; row_index >= top_row; row_index--)
         printf("\t%d", arr[row_index][col_index]);
       left_col ++;
     }
+    // Change traversal direction
     cur_dir = (cur_dir + 1) % MAX_DIRS;
   }
 }
 
 // Diagonal Matrix Display Implementation
 void diagonal_print(int arr[][MAX_COLS], int rows, int cols) {
-  int min_row = 0;
-  int max_row = (rows - 1);
-  int min_col = 0;
-  int max_col = (cols - 1);
+  int min_row = 0;          // top    row
+  int max_row = (rows - 1); // bottom row
+  int min_col = 0;          // left   column
+  int max_col = (cols - 1); // right  column
   int row_index, col_index;
   printf("\nDiagonal Print: \n");
+  // Fix left side column
   for(col_index=min_col, row_index=min_row; row_index <= (max_row - 1); row_index++){
     int row, col;
     for(row = row_index, col = col_index; (row >= min_row) && (col <= max_col); row--, col++)
       printf("\t%d", arr[row][col]);
     printf("\n");
   }
+  // Fix bottom side row
   for(row_index=max_row, col_index=min_col; col_index <= max_col; col_index++){
     int row, col;
     for(row = row_index, col = col_index; (row >= min_row) && (col <= max_col); row--, col++)
@@ -107,8 +138,8 @@ void diagonal_print(int arr[][MAX_COLS], int rows, int cols) {
   }
 }
 
-// ZigZag Matrix Display Implementation
-void zigzag_print(int arr[][MAX_COLS], int rows, int cols) {
+// Horizontal ZigZag Matrix Display Implementation
+void zigzag_horizontal(int arr[][MAX_COLS], int rows, int cols) {
   int row_index, col_index;
   int max_rows = (rows - 1);
   int max_cols = (cols - 1);
@@ -126,10 +157,208 @@ void zigzag_print(int arr[][MAX_COLS], int rows, int cols) {
   }
 }
 
+// Vertical ZigZag Matrix Display Implementation
+void zigzag_vertical(int arr[][MAX_COLS], int rows, int cols) {
+  int row_index, col_index;
+  int max_rows = (rows - 1);
+  int max_cols = (cols - 1);
+  printf("\nZigzag Print: \n");
+  for(col_index = 0; col_index <= max_cols; col_index++) {
+    if(col_index % 2 == 0) {
+      // Downward print
+      for(row_index = 0; row_index <= max_rows; row_index++)
+          printf("\t%d", arr[row_index][col_index]);
+    }
+    else {
+      // Upward print
+      for(row_index = max_rows; row_index >= 0; row_index--)
+          printf("\t%d", arr[row_index][col_index]);
+    }
+    printf("\n");
+  }
+}
+
+// Utility to display square matrix
+void display_square_matrix(int arr[][TBL_COLS], int rows, int cols) {
+  printf("\nSqaure Matrix: \n");
+  for(int i=0; i < rows; i++) {
+    for(int j=0; j < cols; j++) {
+      printf("\t%d", arr[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+// Matrix multiplication
+void matrix_multiplication(int mat1[][TBL_COLS], int mat2[][TBL_COLS]) {
+  int result[TBL_ROWS][TBL_COLS];
+  for(int i = 0; i < TBL_ROWS; i++) {
+    for (int j = 0; j < TBL_COLS; j++) {
+      result[i][j] = 0;
+      for(int k = 0; k < TBL_COLS; k++) {
+        result[i][j] = result[i][j] + (mat1[i][k] * mat2[k][j]);
+      }
+    }
+  }
+  display_square_matrix(result, TBL_ROWS, TBL_COLS);
+}
+
+// Helper function to visit neighbours of island
+void visit_neighbors(int bin[][BIN_COLS], int row, int col) {
+  if((row < BIN_ROWS) && (row >= 0) &&
+     (col < BIN_COLS) && (col >= 0) &&
+     (bin[row][col] != 0)) {
+    // Mark array index as visited
+    bin[row][col] = 0;
+    // Use static defined neighbours
+    static int rowmap[] = {-1,  -1,  -1,   0,   0,   1,   1,   1};
+    static int colmap[] = { 0,  -1,   1,  -1,   1,   0,  -1,   1};
+    // Recursive call for all 8 neighbours
+    for(int n = 0; n < 8; n++) {
+      visit_neighbors(bin, (row + rowmap[n]), (col + colmap[n]));
+    }
+    // Visit 8 neighbours (north, east, south, west, NE, SE, NW, SW)
+    // visit_neighbors(bin, row + 1,  col);       Increment row only
+    // visit_neighbors(bin, row - 1,  col);       Decrement row only
+    // visit_neighbors(bin, row,      col + 1);   Increment col only
+    // visit_neighbors(bin, row,      col - 1);   Increment col only
+    // visit_neighbors(bin, row + 1,  col + 1);   Increment both
+    // visit_neighbors(bin, row - 1,  col - 1);   Decrement both
+    // visit_neighbors(bin, row + 1,  col - 1);   Increment row, Decrement col
+    // visit_neighbors(bin, row - 1,  col + 1);   Decrement row, Increment col
+  }
+}
+
+// Function to count number of islands
+void count_islands(int bin[BIN_ROWS][BIN_COLS]) {
+  int counter = 0;
+  for(int row = 0; row < BIN_ROWS; row++) {
+    for(int col = 0; col < BIN_COLS; col++) {
+      if(bin[row][col] == 1) {
+        counter++;
+        visit_neighbors(bin, row, col);
+      }
+    }
+  }
+  printf("\nCounter: %d", counter);
+}
+
+// Utility to flood fill matrix
+void flood_fill(int clr[CLR_ROWS][CLR_COLS], int row, int col, int prev, int next) {
+  if((row < CLR_ROWS) && (row >= 0) &&
+     (col < CLR_COLS) && (col >= 0) &&
+     (clr[row][col] == prev)) {
+    // Fill with new color
+    clr[row][col] = next;
+    // Use static defined neighbours
+    static int rowfill[] = { 1,  -1,   0,   0};
+    static int colfill[] = { 0,   0,   1,  -1};
+    // Recursive call for only 4 neighbours
+    for(int n = 0; n < 4; n++) {
+      flood_fill(clr, (row + rowfill[n]), (col + colfill[n]), prev, next);
+    }
+    // Visit 4 neighbours (north, south, west, east)
+    // flood_fill(bin, row + 1,  col);       Increment row only
+    // flood_fill(bin, row - 1,  col);       Decrement row only
+    // flood_fill(bin, row,      col + 1);   Increment col only
+    // flood_fill(bin, row,      col - 1);   Increment col only
+  }
+}
+
+void paint_function(int clr[CLR_ROWS][CLR_COLS], int row, int col, int next) {
+  if((row < CLR_ROWS) && (row >= 0) &&
+     (col < CLR_COLS) && (col >= 0) &&
+     (clr[row][col] != next)) {
+      int prev = clr[row][col];
+      flood_fill(clr, row, col, prev, next);
+  }
+}
+
+// Display matrix with color codes
+void display_paint(int clr[CLR_ROWS][CLR_COLS]) {
+  int i, j;
+  printf("\nPicture Matrix: \n");
+  for(i=0; i < CLR_ROWS; i++) {
+    for(j=0; j < CLR_COLS; j++) {
+      printf("\t%d", clr[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+// Input matrix - 5 x 4
+int arr[MAX_ROWS][MAX_COLS] = {
+  {2, 4, 6, 8},
+  {5, 9, 12, 16},
+  {9, 5, 11, 2},
+  {2, 11, 5, 9},
+  {3, 2, 1, 8},
+};
+
+int mat1[TBL_ROWS][TBL_COLS] = {
+  {1, 2, 3},
+  {2, 1, 2},
+  {1, 4, 1},
+};
+
+int mat2[TBL_ROWS][TBL_COLS] = {
+  {5, 2, 1},
+  {4, 1, 1},
+  {2, 3, 1},
+};
+
+int bin[BIN_ROWS][BIN_COLS] = {
+  {1, 1, 0, 0, 0},
+  {0, 1, 0, 0, 1},
+  {1, 0, 0, 1, 1},
+  {0, 0, 0, 0, 0},
+  {1, 0, 1, 0, 1}
+};
+
+int clr[CLR_ROWS][CLR_COLS] = {
+  {1, 1, 1, 1, 1, 1, 1, 1},
+  {1, 1, 1, 1, 1, 1, 0, 0},
+  {1, 0, 0, 1, 1, 0, 1, 1},
+  {1, 3, 3, 3, 3, 0, 1, 0},
+  {1, 1, 1, 3, 3, 0, 1, 0},
+  {1, 1, 1, 3, 3, 3, 3, 0},
+  {1, 1, 1, 1, 1, 3, 1, 1},
+  {1, 1, 1, 1, 1, 3, 3, 1},
+};
+
+
+// Helper function to get determinant of 2D matrix
+// Top Row, Bottom Row, Left Column, Right Column
+int matrix_2determinant(int** arr, int i, int j, int x, int y) {
+  int val = (arr[i][x] * arr[j][y]) - (arr[j][x] * arr[i][y]);
+  return val;
+}
+
+// Helper function to get determinant of 3D matrix
+int matrix_3determinant(int** arr, int row, int col) {
+  int value = 0;
+  int sign = 1;
+  for(int j = 0; j < col; j++) {
+    int temp = sign * arr[0][j];
+    sign = sign * -1;
+    int x = (j + 1) % col;
+    int y = (x + 1) % col;
+    // Always ensure sorted column order
+    if(x > y) {
+      int val = x;
+      x = y;
+      y = val;
+    }
+    // Specify rows and columns
+    value = value + temp * matrix_2determinant(arr, 1, 2, x, y);
+  }
+  return value;
+}
+
 // Main driver function
 int main(int argc, char* argv[]) {
-  print_matrix(arr, MAX_ROWS, MAX_COLS);
-  //spiral_print(arr, MAX_ROWS, MAX_COLS);
-  zigzag_print(arr, MAX_ROWS, MAX_COLS);
+  display_paint(clr);
+  paint_function(clr, 4, 4, 2);
+  display_paint(clr);
   return 0;
 }
--- LinearDS/single_linked_list.cpp
+++ LinearDS/single_linked_list.cpp
@@ -1,130 +1,430 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-struct Node{
-	int value;
-	struct Node* next;
+struct Node {
+  int value;
+  struct Node* next;
 };
+
 typedef struct Node NODE;
 
-void display_list(NODE* head, const char* title) {
-	NODE* ptr = NULL;
-	printf("\n%s: ", title);
-	for(ptr = head; ptr != NULL; ptr = ptr->next) {
-		printf("%d%s", ptr->value, ((ptr->next)? "->" : ""));
-	}
+// Create Node
+NODE* create_node(const int value) {
+  NODE* ptr = (NODE*)malloc(sizeof(NODE));
+  ptr->value = value;
+  ptr->next = NULL;
+  return ptr;
 }
 
+// Display linked list
+void display_list(NODE* head) {
+  NODE *ptr = NULL;
+  printf("\nList:");
+  for(ptr = head; ptr != NULL; ptr = ptr->next) {
+    const char* line = (ptr->next != NULL) ? "--->" : "";
+    printf(" %d %s", ptr->value, line);
+  }
+}
+
+// Delete entire linked list
 void delete_list(NODE* head) {
-	NODE *ptr, *prev;
-	for(prev = head, ptr = ((head)?head->next:NULL); ptr != NULL; prev = ptr, ptr = ptr->next) {
-		free(prev);
-	}
-	free(prev);
-	head = NULL;
+  NODE *prev, *ptr;
+  printf("\nCleanup list");
+  for(prev = head, ptr = ((prev)?prev->next:NULL); ptr != NULL; prev = ptr, ptr = ptr->next) {
+    printf("\nFreeing: %d", prev->value);
+    free(prev);
+  }
+  if(prev != NULL) {
+    printf("\nFreeing: %d", prev->value);
+    free(prev);
+    prev = NULL;
+  }
 }
 
-NODE* create_node(int value) {
-	NODE* temp = (NODE*) malloc (sizeof(NODE));
-	if(temp != NULL) {
-		temp->value = value;
-		temp->next = NULL;
-	}
-	return temp;
+// Insert node to end of linked list
+NODE* push_back(NODE* head, const int value) {
+  NODE* temp = create_node(value);
+  if(head == NULL) {
+    head = temp;
+  } else {
+    NODE* ptr = NULL;
+    for(ptr = head; ptr->next != NULL; ptr = ptr->next);
+    ptr->next = temp;
+  }
+  return head;
 }
 
-void push_back(NODE** head_ref, int value) {
-	NODE *temp = create_node(value);
-	if(*head_ref == NULL) {
-		*head_ref = temp;
-	}
-	else {
-		NODE* ptr;
-		for(ptr = (*head_ref); ptr->next != NULL; ptr = ptr->next);
-		ptr->next = temp;
-	}
+// Insert after node with specific key value
+void insert_after(NODE** headptr, const int keyval, const int newval) {
+  NODE* head = *headptr;
+  NODE* ptr = NULL;
+  for(ptr = head; ptr != NULL && ptr->value != keyval; ptr = ptr->next);
+  if(ptr != NULL) {
+    printf("\nInserting %d after %d", newval, keyval);
+    NODE* temp = create_node(newval);
+    temp->next = ptr->next;
+    ptr->next = temp;
+  } else {
+    printf("\nKey value %d does not exist", keyval);
+  }
 }
 
-// NODE* reverse_list(NODE* head) {
-// 	NODE *ptr1, *ptr2, *ptr3;
-// 	ptr1 = head;
-// 	ptr2 = (ptr1) ? ptr1->next : NULL;
-// 	ptr3 = (ptr2) ? ptr2->next : NULL;
-//
-// 	if(ptr1 != NULL)
-// 		ptr1->next = NULL;
-// 	else
-// 		return ptr1;
-//
-// 	while(ptr3 != NULL) {
-// 		ptr2->next = ptr1;
-// 		ptr1 = ptr2;
-// 		ptr2 = ptr3;
-// 		ptr3 = ptr3->next;
-// 	}
-//
-// 	if(ptr2 != NULL) {
-// 		ptr2->next = ptr1;
-// 		head = ptr2;
-// 	}
-// 	return head;
-// }
-//
-// NODE* reverse_sublist(NODE* head, int m, int n) {
-// 	NODE *prev, *next, *start, *end, *ptr;
-// 	int index;
-// 	NODE dummy;
-// 	dummy.next = head;
-// 	for(ptr = &dummy, index = 0; ptr != NULL; ptr = ptr->next, index++) {
-// 		if(index == (m - 1)) {
-// 			prev = ptr;
-// 			start = prev->next;
-// 		}
-// 		else if(index == n) {
-// 			end = ptr;
-// 			next = end->next;
-// 			end->next = NULL;
-// 			break;
-// 		}
-// 	}
-// 	if(index == n) {
-// 		NODE* reverse = reverse_list(start);
-// 		prev->next = reverse;
-// 		start->next = next;
-// 	}
-// 	return dummy.next;
-// }
+// Insert before node with specific key value
+void insert_before(NODE** headptr, const int keyval, const int newval) {
+  NODE *head = *headptr;
+  NODE *prev, *ptr;
+  for(prev = head, ptr = head; ptr != NULL && ptr->value != keyval; prev = ptr, ptr = ptr->next);
+  if(ptr != NULL) {
+    printf("\nInserting %d before %d", newval, keyval);
+    NODE* temp = create_node(newval);
+    if (ptr == head) {
+      temp->next = prev;
+      head = temp;
+    } else {
+      temp->next = prev->next;
+      prev->next = temp;
+    }
+  } else {
+    printf("\nKey value %d does not exist", keyval);
+  }
+  *headptr = head;
+}
 
+// Delete node with specific key value
+void delete_node(NODE** headptr, const int keyval) {
+  NODE *head = *headptr;
+  NODE *prev, *ptr;
+  for(prev = head, ptr = head; ptr != NULL && ptr->value != keyval; prev = ptr, ptr = ptr->next);
+  if(ptr != NULL) {
+    printf("\nDeleting %d", keyval);
+    NODE* temp = NULL;
+    if (ptr == head) {
+      temp = ptr;
+      head = ptr->next;
+      printf("\nNew head: %d", head->value);
+    } else {
+      temp = ptr;
+      prev->next = ptr->next;
+    }
+    free(temp);
+    temp = NULL;
+  } else {
+    printf("\nKey value %d does not exist", keyval);
+  }
+  *headptr = head;
+}
+
+// Delete all matching nodes
+void delete_matching_nodes(NODE** headptr, const int keyval) {
+  NODE dummy;
+  dummy.value = 0;
+  dummy.next = *headptr;
+  NODE *main = &dummy;
+  NODE *prev = main->next;
+  NODE *ptr = (prev != NULL)? prev->next : NULL;
+  while (ptr != NULL) {
+    if(prev->value == keyval) {
+      NODE* temp = prev;
+      main->next = prev->next;
+      free(temp);
+      temp = NULL;
+    } else {
+      main = prev;
+    }
+    prev = ptr;
+    ptr = ptr->next;
+  }
+  if ((prev != NULL) && (prev->value == keyval)) {
+    NODE* temp = prev;
+    main->next = prev->next;
+    free(temp);
+    temp = NULL;
+  }
+  *headptr = dummy.next;
+}
+
+// Get last node of linked list
+NODE* get_last_node(NODE* head) {
+  NODE* ptr = NULL;
+  for(ptr = head; ((ptr != NULL) && (ptr->next != NULL)); ptr = ptr->next);
+  return ptr;
+}
+
+// Get middle node of linked list
+NODE* get_middle_node(NODE* head) {
+  NODE* slow = head;
+  NODE* fast = (slow)?slow->next:NULL;
+  while(slow && fast && fast->next) {
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+  return slow;
+}
+
+// Get node with specific key value
+NODE* get_value_node(NODE* head, const int value) {
+  NODE* ptr = NULL;
+  for(ptr = head; (ptr != NULL) && (ptr->value != value); ptr = ptr->next);
+  return ptr;
+}
+
+// Get node with specific index
+NODE* get_index_node(NODE* head, const int index) {
+  NODE* ptr = NULL;
+  int counter = 0;
+  for(counter = 1, ptr = head; (ptr != NULL) && (counter != index); counter++, ptr = ptr->next);
+  return ptr;
+}
+
+// Convery array to linked list
+NODE* array_to_list(int arr[], int size) {
+  NODE* head = NULL;
+  for(int index = 0; index < size; index++) {
+    head = push_back(head, arr[index]);
+  }
+  return head;
+}
+
+// Break linked list and alternate merge both sublists
+NODE* middle_alternate_merge(NODE* head) {
+  NODE* middle = get_middle_node(head);
+  NODE* head1 = head;
+  NODE* head2 = middle->next;
+  middle->next = NULL;
+  NODE* ptr1 = head1;
+  NODE* ptr2 = head2;
+  NODE* result = ptr1;
+  while(ptr1 && ptr2) {
+    NODE* temp1 = ptr1->next;
+    NODE* temp2 = ptr2->next;
+    ptr1->next = ptr2;
+    ptr2->next = temp1;
+    ptr1 = temp1;
+    ptr2 = temp2;
+  }
+  if(ptr1) {
+    ptr1->next = NULL;
+  }
+  if(ptr2) {
+    ptr2->next = NULL;
+  }
+  return result;
+}
+
+// Merge two sorted linked lists into one
+NODE* merge_sort_lists(NODE* head1, NODE* head2) {
+  NODE* ptr1 = head1;
+  NODE* ptr2 = head2;
+  NODE* main = NULL;
+  if(ptr1 && ptr2) {
+    main = (ptr1->value <= ptr2->value) ? ptr1 : ptr2;
+  } else {
+    main = (ptr1) ? ptr1 : ptr2;
+  }
+  while(ptr1 && ptr2) {
+    NODE* temp1 = ptr1->next;
+    NODE* temp2 = ptr2->next;
+    if(ptr1->value < ptr2->value) {
+      // since ptr1 is smaller (consume it)
+      // ptr1->next = smaller value between ptr2 and temp1
+      if(temp1 && temp1->value <= ptr2->value) {
+        ptr1->next = temp1;
+      } else {
+        ptr1->next = ptr2;
+      }
+      // cosumed ptr1 so increment to next
+      ptr1 = temp1;
+    }
+    else if(ptr1->value > ptr2->value) {
+      // since ptr2 is smaller (consume it)
+      // ptr2->next = smaller value between ptr1 and temp2
+      if(temp2 && temp2->value < ptr1->value) {
+        ptr2->next = temp2;
+      } else {
+        ptr2->next = ptr1;
+      }
+      // cosumed ptr2 so increment to next
+      ptr2 = temp2;
+    } else {
+      printf("\nPTR matching %d %d", ptr1->value, ptr2->value);
+      // consume ptr1 and ptr2
+      ptr1->next = ptr2;
+      if(temp1 && temp2 && (temp1->value <= temp2->value)) {
+        ptr2->next = temp1;
+      } else {
+        ptr2->next = temp2;
+      }
+      // cosumed ptr1 and ptr2 so increment to next
+      ptr1 = temp1;
+      ptr2 = temp2;
+    }
+  }
+  return main;
+}
+
+// Reverse linked list
 NODE* reverse_linked_list(NODE* head) {
-	NODE* ptr1 = (head) ? head : NULL;
-	NODE* ptr2 = (ptr1) ? ptr1->next : NULL;
-	NODE* ptr3 = (ptr2) ? ptr2->next : NULL;
-	if(ptr1 && ptr2) {
+  NODE* ptr1 = (head) ? head : NULL;
+  NODE* ptr2 = (ptr1) ? ptr1->next : NULL;
+  NODE* ptr3 = (ptr2) ? ptr2->next : NULL;
+  NODE* main = ptr1;
+  if(ptr1 && ptr2) {
 		ptr1->next = NULL;
 		while(ptr2 && ptr2->next) {
-			ptr2->next = ptr1;
-			ptr1 = ptr2;
-			ptr2 = ptr3;
-			ptr3 = ptr2->next;
-		}
-		ptr2->next = ptr1;
-		head = ptr2;
-	}
-	return head;
+      ptr2->next = ptr1;
+      ptr1 = ptr2;
+      ptr2 = ptr3;
+      ptr3 = ptr3->next;
+    }
+    ptr2->next = ptr1;
+    main = ptr2;
+  }
+  return main;
 }
 
-int main(int argc, char* argv[]) {
-	NODE* head = NULL;
-	push_back(&head, 3);
-	push_back(&head, 4);
- 	push_back(&head, 5);
-	push_back(&head, 6);
-	push_back(&head, 7);
-	push_back(&head, 8);
-	display_list(head, "Linked List");
-	head = reverse_linked_list(head);
-	display_list(head, "Reversed List");
-	// head = reverse_sublist(head, 1, 5);
-	// display_list(head, "Reversed Sublist");
-	delete_list(head);
+// Break linked list and merge reversed sublists
+NODE* reverse_merge_list(NODE* head) {
+  NODE* middle = get_middle_node(head);
+  middle->next = reverse_linked_list(middle->next);
+  return head;
+}
+
+// Find loop node in linked list
+NODE* find_loop_node(NODE* head) {
+  NODE* slow = head;
+  NODE* fast = (slow) ? slow->next : NULL;
+  while(slow && fast && fast->next) {
+    if(slow == fast) {
+      return slow;
+    }
+    slow = slow->next;
+    fast = fast->next->next;
+  }
+  return NULL;
+}
+
+// Detect and break loop in linked list
+void detect_break_loop(NODE* head) {
+  NODE* loop = find_loop_node(head);
+  if(loop == NULL) {
+    printf("\nNo loop exists");
+    return;
+  }
+  printf("\nNode %d is one of the loop nodes", loop->value);
+  NODE* ptr1 = head;
+  NODE* ptr2 = loop;
+  while(ptr2->next != ptr1) {
+    ptr1 = ptr1->next;
+    ptr2 = ptr2->next;
+  }
+  printf("\nLoop Start: %d", ptr1->value);
+  printf("\nLoop End: %d", ptr2->value);
+  ptr2->next = NULL;
+}
+
+// Reverse linked list from M to N
+NODE* reverse_sublist_range(NODE* head, const int m, const int n) {
+  if((m < 0)||(n < 0)||(m >= n)) {
+    printf("\nInvalid range %d-%d", m, n);
+    return head;
+  }
+  NODE dummy;
+  dummy.value = 0;
+  dummy.next = head;
+  NODE *prev, *next, *start, *end, *ptr;
+  int index;
+  // Iterate over linked list
+  for(index = 0, ptr = &dummy; ptr != NULL; index++, ptr = ptr->next) {
+    if(index == (m-1)) {
+      prev = ptr;
+      start = ptr->next;
+    }
+    if(index == n) {
+      next = ptr->next;
+      end = ptr;
+      end->next = NULL;
+      break;
+    }
+  }
+  if(index == n) {
+    prev->next = reverse_linked_list(start);
+    start->next = next;
+  }
+  return dummy.next;
+}
+
+// Reverse linked list in groups of K
+NODE* reverse_sublist_group(NODE* head, const int group) {
+  NODE* main = NULL;
+  NODE* chain = NULL;
+  NODE* ptr = head;
+  NODE *start, *prev;
+  while (ptr != NULL) {
+    prev  = NULL;
+    start = ptr;
+    int count = group;
+    // Reverse group
+    while (ptr && (count > 0)) {
+      NODE* temp = ptr->next;
+      ptr->next = prev;
+      prev = ptr;
+      ptr = temp;
+      count --;
+    }
+    // Set new head
+    if(main == NULL) {
+      main = prev;
+    }
+    // Chain previous start to current previous (last)
+    if(chain != NULL) {
+      chain->next = prev;
+    }
+    // Reset chain to current start
+    chain = start;
+  }
+  return main;
+}
+
+int main() {
+  int arr[] = {1, 2, 3, 4, 5};
+  int size = sizeof(arr)/sizeof(arr[0]);
+  NODE* head = array_to_list(arr, size);
+  display_list(head);
+  head = reverse_sublist_group(head, 1);
+  display_list(head);
+  delete_list(head);
+
+  // head = reverse_merge_list(head);
+  // display_list(head);
+
+  // NODE* temp = get_index_node(head, 3);
+  // NODE* last = get_last_node(head);
+  // printf("\nLoop induced %d --> %d", last->value, temp->value);
+  // last->next = temp;
+  // detect_break_loop(head);
+  // //last->next = NULL;
+  // delete_list(head);
+  // int arr1[] = {1, 5, 9, 12};
+  // int size1 = sizeof(arr1)/sizeof(arr1[0]);
+  // NODE* head1 = array_to_list(arr1, size1);
+  // display_list(head1);
+  //
+  // int arr2[] = {2, 4, 7, 10};
+  // int size2 = sizeof(arr2)/sizeof(arr2[0]);
+  // NODE* head2 = array_to_list(arr2, size2);
+  // display_list(head2);
+  //
+  // NODE* head = merge_sort_lists(head1, head2);
+  // display_list(head);
+  // delete_list(head);
+
+  //merge_sort_lists(head1, head2);
+  // NODE* last = get_last_node(head);
+  // printf("\nLast Node: %d", (last)?last->value:-1);
+  // NODE* middle = get_middle_node(head);
+  // printf("\nMiddle Node: %d", (middle)?middle->value:-1);
+  // head = middle_alternate_merge(head);
+  // display_list(head);
+  // delete_list(head);
+  return 0;
 }
